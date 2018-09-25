24b140257bcc38029b92b519fae39c6ad26f9453
michaelstrezishar <michael.strezishar@gmail.com>
2018-09-25 13:59:15 -0500

    movies exercise

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -1,4 +1,21 @@
 package movies;
 
 public class Movie {
+   private String name;
+   private String category;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+    public Movie(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
+
+
 }
