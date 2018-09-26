223d5e62d31f0032d9be8e1ec467072da2ba134e
michaelstrezishar <michael.strezishar@gmail.com>
2018-09-26 14:55:03 -0500

    Shapes exercise

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -13,7 +13,7 @@ public class ShapesTest {
 //        System.out.println(box2.getPerimeter());
 //        System.out.println(box2.getArea());
 
-        Shape myShape = new Rectangle(200, 300);
+        Shape myShape = new Rectangle(5, 4);
         showShapeInfo(myShape);
         myShape = new Square(5);
         showShapeInfo(myShape);
@@ -21,9 +21,15 @@ public class ShapesTest {
         public static void showShapeInfo(Measurable shape) {
             System.out.printf("Perimeter = %f Area = %f\n", shape.getPerimeter(), shape.getArea());
         }
+//    Every class that implements an interface must implement each method declared in the interface. Since
+//  the Rectangle class implements the Measurable interface, it must define the implementation of the getPerimeter()
+//  method.
+//
+//   What happens if you try to call the getLength or getWidth methods of the myShape variable? Why?
+//   Because the Measurable interface does not declare the getLength or getWidth methods, it will not be available
+//   to any instances that are declared type Measurable.
 
-
-
+//
 
 //        Rectangle rect = new Rectangle(5, 6);
 //        System.out.println(rect.getArea());
